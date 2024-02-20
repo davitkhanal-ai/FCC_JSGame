ab7f016d8553ad2263685f3b65ed3dad6a15a6b4
davit@aspire3 <khanaldavit@gmail.com>
2024-02-20 21:46:22 +0545

    js updated

--- freecodecamp/script.js
+++ freecodecamp/script.js
@@ -1,3 +1,4 @@
+// declare empty values and other default
 let xp = 0;
 let health = 100;
 let gold = 50;
@@ -5,7 +6,68 @@ let currentWeapon = 0;
 let fighting;
 let monsterHealth;
 let inventory = ["stick"];
+const location = [
+  {
+    name: "town square",
+    "button text": ["Go to store", "Go to cave", "Fight dragon"],
+    "button functions": [goStore, goCave, fightDragon],
+    text: "You are in the town square. You see a sign that says \"Store\"."
+  },
+  {
+    name: "store",
+    "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
+    "button functions": [buyHealth, buyWeapon, goTown],
+    text: 'You enter the store.',
+  },
+];
 
-const button1  = document.querySelector("#button1");
-const button2  = document.querySelector("#button2");
-const button3  = document.querySelector("#button3");
+// declare button values
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+
+// declare button text
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterName = document.querySelector("#monsterName");
+const text = document.querySelector("#text");
+const monsterHealthText = document.querySelector("#monsterHealth");
+
+// onclick functions for button
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+
+//functions
+function goTown() {
+    update(location)
+}
+
+function goStore() {
+ 
+}
+
+function goCave() {
+  console.log("Going to cave.");
+}
+
+function fightDragon() {
+  console.log("Fighting dragon.");
+}
+
+function buyHealth() {}
+function buyWeapon() {}
+function goTown() {}
+
+function update(location) {
+    button1.innerText = "Go to store";
+    button2.innerText = "Go to cave";
+    button3.innerText = "Fight dragon";
+    button1.onclick = goStore;
+    button2.onclick = goCave;
+    button3.onclick = fightDragon;
+    text.innerText =
+      'You are in the town square. You see a sign that says "Store".';
+}
