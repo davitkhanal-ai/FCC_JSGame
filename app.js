1a6b7863c2bf514f497866c74eed9ca1396f49cb
davitkhanal-ai <khanaldavit@gmail.com>
2024-02-25 20:58:50 +0545

    remove unwanted

--- app.js
+++ app.js
@@ -5,16 +5,6 @@ const expressLayouts = require("express-ejs-layouts");
 
 // middlewares
 app.use(express.urlencoded({ extended: true }));
-app.use(expressLayouts);
-app.use(express.static(__dirname + "/public"));
-
-// to get from routers
-const gameRoutes = require("./server/routes/gameRoutes");
-app.get("/", gameRoutes);
-
-// to layouts
-app.set("layout", path.join(__dirname, "views/layouts/main.ejs"));
-app.set("view engine", "ejs");
 
 //server boot
 app.listen(8000, () => {
