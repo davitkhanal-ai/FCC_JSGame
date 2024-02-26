bb924eb1463150b49e62bf91d53d1e024156388e
davit@aspire3 <khanaldavit@gmail.com>
2024-02-26 23:42:09 +0545

    changes made for login

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const db = require("./server/database/db");
 const rpgRouter = require("./server/routes/routes");
 
 // middlewares
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // database
